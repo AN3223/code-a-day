14eda1c729efe437012747bec8f59a6d22e6684c
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-08-05 17:35:02 -0500

    Create dontrunthis.js

---
+++ Day 21/dontrunthis.js
@@ -0,0 +1,4 @@
+setInterval(() => document.body.style.background = "white", 1e3)
+setInterval(() => document.body.style.background = "red", 1e3 / 2)
+setInterval(() => document.body.style.background = "blue", 1e3 / 3)
+setInterval(() => document.body.style.background = "green", 1e3 / 4)
